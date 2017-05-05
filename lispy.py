8806fb668976ef3f8124461e2a2058845f093181
Manoj Mohan <m.manoj@linuxmail.org>
2017-05-05 13:33:57 +0530

    added build_list function which takes the un parsed list of char and makes AST out of it

--- lispy.py
+++ lispy.py
@@ -1,9 +1,9 @@
 def scanner(input_str):
-    '''Scans the input string from standard input and replaces '(' with '['
-        and ')' with ']' '''
+    '''Scans the input string from standard input and replaces '(' with ' [ '
+        and ')' with ' ] ' '''
     split_tokens = []
-    input_str = input_str.replace('(', ' [ ')
-    input_str = input_str.replace(')', ' ] ')
+    input_str = input_str.replace('(', ' ( ')
+    input_str = input_str.replace(')', ' ) ')
 
     split_tokens = input_str.split()
 
@@ -49,11 +49,12 @@ def parser(un_parsed_list_of_tokens):
 
         return True
 
+
     for x in range(0,len(un_parsed_list_of_tokens)):
 
-        if(un_parsed_list_of_tokens[x] == '['):
+        if(un_parsed_list_of_tokens[x] == '('):
             parenthesis.append('(')
-        elif(un_parsed_list_of_tokens[x] == ']'):
+        elif(un_parsed_list_of_tokens[x] == ')'):
             parenthesis.append(')')
 
         parsed_list_of_tokens.append(item_parser(un_parsed_list_of_tokens[x]))
@@ -62,12 +63,42 @@ def parser(un_parsed_list_of_tokens):
     if (is_parenthesis_valid(parenthesis) == False):
         raise SyntaxError("Syntax Error invalid parenthesis")
 
+
     return parsed_list_of_tokens
 
 
+def build_list(un_parsed_tokens):
+    new_list = []
+    x = 0
+    if un_parsed_tokens[x] == '(':
+        while (un_parsed_tokens[x+1] != ')'):
 
-input_string_main = input()
-print ('parser output : ', parser(scanner(input_string_main)))
+            if(un_parsed_tokens[x+1] == '('):
+                sub_list = build_list(un_parsed_tokens[x+1:])   # recursive function call
+                new_list.append(sub_list)
+                x += (len(sub_list) + 2)
+                continue
 
+            new_list.append(un_parsed_tokens[x+1])
+            x += 1
+        return new_list
+
+
+    new_list.append(build_list())
+
+def operator(s_expression):
+    pass
+
+
+def evaluator(s_expression):
+    if (s_expression[0] == '+' or '-' or '*' or '/'):
+        result = operator(s_expression)
+        return result
+
+
+if __name__ == '__main__':
+
+    input_string_main = input()
+    print ('parser output : ', parser(scanner(input_string_main)))
 
 
